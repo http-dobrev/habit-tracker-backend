ca3ec32a6df37ebcf824c45857518456c2135734
Dobrev,Konstantin K.N. <529830@student.fontys.nl>
2026-03-12 13:50:22 +0100

    Added 2 helper classes for testing db connection

--- src/main/java/com/konstantin/habittracker/controller/TestController.java
+++ src/main/java/com/konstantin/habittracker/controller/TestController.java
@@ -3,8 +3,29 @@ package com.konstantin.habittracker.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.konstantin.habittracker.TestEntity;
+import com.konstantin.habittracker.TestRepository;
+
 @RestController
 public class TestController {
+
+    private final TestRepository repository;
+
+    public TestController(TestRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/db-test")
+    public String testDatabase() {
+
+        TestEntity entity = new TestEntity();
+        entity.setMessage("Database connection works");
+
+        repository.save(entity);
+
+        return "Saved to database";
+    }
+
     @GetMapping("/")
     public String home() {
         return "Backend is running";
