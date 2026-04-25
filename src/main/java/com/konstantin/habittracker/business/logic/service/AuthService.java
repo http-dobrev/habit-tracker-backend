379e54ea7b3998a28dddcd195131e3c4b3fae5ce
konstantin <konstantindobrev576@gmail.com>
2026-04-25 13:50:09 +0200

    Added auth service class, error exceptions with a handler, errorresponse class

---
+++ src/main/java/com/konstantin/habittracker/business/logic/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.konstantin.habittracker.business.logic.service;
+
+import com.konstantin.habittracker.dto.request.RegisterRequest;
+import com.konstantin.habittracker.dto.response.AuthResponse;
+import com.konstantin.habittracker.exception.EmailAlreadyExistsException;
+import com.konstantin.habittracker.exception.InvalidRequestException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+}
